394be36b600793aa6765b34ee61ceaad46c60801
GreenValley <samuel.pjm.slade@live.co.uk>
2013-07-18 21:14:39 +0100

    Written test implementations for testing the parsing logic of the CommandLineParser.

--- Slade/Slade.Commands.Tests/Parsing/CommandLineParserTests.cs
+++ Slade/Slade.Commands.Tests/Parsing/CommandLineParserTests.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Slade.Commands.Parsing;
+using System.Linq;
 
 namespace Slade.Commands.Tests.Parsing
 {
@@ -21,6 +23,18 @@ namespace Slade.Commands.Tests.Parsing
 		[TestMethod]
 		public void Parse_AllowSwitchesDoubleHyphenPrefix_OneSwitchCommand()
 		{
+			var parser = new CommandLineParser();
+			parser.RuleSet.AllowSwitches = true;
+			parser.RuleSet.Prefixes = CommandPrefixes.DoubleHyphen;
+
+			string[] arguments = new string[1] { "/switch" };
+			var commands = parser.Parse(arguments).ToArray();
+
+			Assert.AreEqual(1, commands.Length);
+
+			var command = commands[0];
+			Assert.AreEqual("switch", command.Key);
+			Assert.IsFalse(command.HasValue);
 		}
 
 		/// <summary>
@@ -40,6 +54,20 @@ namespace Slade.Commands.Tests.Parsing
 		[TestMethod]
 		public void Parse_ForwardSlashPrefixEqualsSeparator_OneCommandWithKeyAndValue()
 		{
+			var parser = new CommandLineParser();
+			parser.RuleSet.Prefixes = CommandPrefixes.ForwardSlash;
+			parser.RuleSet.Separators = CommandSeparators.Equals;
+
+			string[] arguments = new string[1] { "/key=value" };
+			var commands = parser.Parse(arguments).ToArray();
+
+			Assert.AreEqual(1, commands.Length);
+
+			var command = commands[0];
+			Assert.AreEqual("key", command.Key);
+			Assert.IsTrue(command.HasValue);
+
+			Assert.AreEqual("value", command.Value);
 		}
 
 		/// <summary>
@@ -60,6 +88,26 @@ namespace Slade.Commands.Tests.Parsing
 		[TestMethod]
 		public void Parse_AllowMultipleValuesForwardSlashPrefixColonSeparator_OneCommandWithKeyAndThreeValues()
 		{
+			var parser = new CommandLineParser();
+			parser.RuleSet.AllowMultipleValues = true;
+			parser.RuleSet.Prefixes = CommandPrefixes.ForwardSlash;
+			parser.RuleSet.Separators = CommandSeparators.Colon;
+
+			string[] arguments = new string[1] { "/key:val1;val2;val3" };
+			var commands = parser.Parse(arguments).ToArray();
+
+			Assert.AreEqual(1, commands.Length);
+
+			var command = commands[0];
+			Assert.AreEqual("key", command.Key);
+			Assert.IsTrue(command.HasValue);
+
+			var commandValue = command.Value as string[];
+			Assert.IsNotNull(commandValue);
+			Assert.AreEqual(3, commandValue.Length);
+			Assert.AreEqual("val1", commandValue[0]);
+			Assert.AreEqual("val2", commandValue[1]);
+			Assert.AreEqual("val3", commandValue[2]);
 		}
 
 		/// <summary>
@@ -80,11 +128,26 @@ namespace Slade.Commands.Tests.Parsing
 		[TestMethod]
 		public void Parse_AllowCombinedValuesSingleHyphenPrefix_TwoCommands()
 		{
+			var parser = new CommandLineParser();
+			parser.RuleSet.AllowCombinedSwitches = true;
+			parser.RuleSet.Prefixes = CommandPrefixes.SingleHyphen;
+
+			string[] arguments = new string[1] { "-ab" };
+			var commands = parser.Parse(arguments).ToArray();
+
+			Assert.AreEqual(2, commands.Length);
+
+			var command = commands[0];
+			Assert.AreEqual("a", command.Key);
+			Assert.IsFalse(command.HasValue);
+
+			command = commands[1];
+			Assert.AreEqual("b", command.Key);
+			Assert.IsFalse(command.HasValue);
 		}
 
 		/// <summary>
 		/// Configuration:
-		///		AllowCombinedValues	True
 		///		Prefixes			SingleHyphen
 		///
 		/// Input:
@@ -100,6 +163,21 @@ namespace Slade.Commands.Tests.Parsing
 		[TestMethod]
 		public void Parse_SingleHyphenPrefix_TwoCommands()
 		{
+			var parser = new CommandLineParser();
+			parser.RuleSet.Prefixes = CommandPrefixes.SingleHyphen;
+
+			string[] arguments = new string[1] { "-a -b" };
+			var commands = parser.Parse(arguments).ToArray();
+
+			Assert.AreEqual(2, commands.Length);
+
+			var command = commands[0];
+			Assert.AreEqual("a", command.Key);
+			Assert.IsFalse(command.HasValue);
+
+			command = commands[1];
+			Assert.AreEqual("b", command.Key);
+			Assert.IsFalse(command.HasValue);
 		}
 	}
 }
