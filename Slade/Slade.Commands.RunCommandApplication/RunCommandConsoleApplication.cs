a6c5c7c8d471674685745cc9235e6521f7930542
samuel.slade <samuel.slade@advancedcomputersoftware.com>
2013-07-31 16:34:53 +0100

    Implemented logic for registration handling.

--- Slade/Slade.Commands.RunCommandApplication/RunCommandConsoleApplication.cs
+++ Slade/Slade.Commands.RunCommandApplication/RunCommandConsoleApplication.cs
@@ -1,5 +1,6 @@
 ﻿using Slade.Commands.Parsing;
 using System;
+using System.Collections.Generic;
 
 namespace Slade.Commands.RunCommandApplication
 {
@@ -9,6 +10,9 @@ namespace Slade.Commands.RunCommandApplication
     /// </summary>
     public class RunCommandConsoleApplication : ConsoleApplication
     {
+        private readonly Dictionary<string, string> mProgramRegistrations =
+            new Dictionary<string, string>(StringComparer.InvariantCultureIgnoreCase);
+
         /// <summary>
         /// Initializes a new instance of the <see cref="RunCommandConsoleApplication" /> class.
         /// </summary>
@@ -18,7 +22,19 @@ namespace Slade.Commands.RunCommandApplication
         public RunCommandConsoleApplication(string[] arguments)
             : base(arguments)
         {
+            Configure(
+                configuration =>
+                {
+                    configuration.AllowMultipleValues = true;
+                    configuration.AllowSwitches = false;
+                    configuration.Prefixes = CommandPrefixes.ForwardSlash;
+                    configuration.Separators = CommandSeparators.Equals;
+                }
+            );
         }
+
+        // TODO: It would be nice to be able to register commands with patterns and descriptions so that the a default help command can be provided
+        // TODO: for all commands and each individual command - using the format of "command help" or "command help parameter".
 
         /// <summary>
         /// Executes the operations contained within the console application implementation.
@@ -32,7 +48,43 @@ namespace Slade.Commands.RunCommandApplication
 
         private void HandleRegistration(CommandResultSet commands)
         {
-            // TODO: Check to see if we have a registration command.
+            string[] registrationParameters;
+
+            if (!commands.TryGetValue<string[]>("register", out registrationParameters))
+            {
+                return;
+            }
+
+            if (registrationParameters.Length != 2)
+            {
+                ConsoleHelper.WriteLine(ConsoleMessageType.Error,
+                                        "Invalid number of values specified for command registration. Please specify the name and program path.");
+
+                return;
+            }
+
+            // Extract the registration name and program path from the registration parameters
+            string registrationName = registrationParameters[0];
+            string programPath = registrationParameters[1];
+
+            VerificationProvider.VerifyValidString(registrationName, "registrationName");
+            VerificationProvider.VerifyValidString(programPath, "programPath");
+
+            // Check we haven't already made a registration with this name
+            // TODO: This information needs to be persisted to some file store.
+            if (mProgramRegistrations.ContainsKey(registrationName))
+            {
+                ConsoleHelper.WriteLine(ConsoleMessageType.Warning,
+                                        "A program has already been registered under the name '{0}' and will be overridden.",
+                                        registrationName);
+            }
+
+            // Store the registration, overriding any existing registrations
+            mProgramRegistrations[registrationName] = programPath;
+
+            ConsoleHelper.WriteLine(ConsoleMessageType.Information,
+                                    "A registration has been successfully made under '{0}' for the path '{1}'.",
+                                    registrationName, programPath);
         }
 
         private void HandleLaunching(CommandResultSet commands)
