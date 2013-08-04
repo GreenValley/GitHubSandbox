bcb9879e79b9cb14234bfcc194979cb253a7dbda
GreenValley <samuel.pjm.slade@live.co.uk>
2013-08-04 16:43:11 +0100

    Saving and loading of application context for the run application is complete.

--- Slade/Slade.Commands.RunCommandApplication/RunCommandConsoleApplication.cs
+++ Slade/Slade.Commands.RunCommandApplication/RunCommandConsoleApplication.cs
@@ -4,101 +4,103 @@ using System.Diagnostics;
 
 namespace Slade.Commands.RunCommandApplication
 {
-    /// <summary>
-    /// Application used to provide a Run command that can be used to register executables and script files
-    /// that can be run through the use of this application by specifying the registered name.
-    /// </summary>
-    public class RunCommandConsoleApplication : ConsoleApplication<IRunCommandApplicationContext>
-    {
-        /// <summary>
-        /// Initializes a new instance of the <see cref="RunCommandConsoleApplication" /> class.
-        /// </summary>
-        /// <param name="applicationContext">An instance of the context required for the current application.</param>
-        /// <param name="arguments">A collection of all arguments passed through to the application
-        /// from the command line.</param>
-        /// <exception cref="ArgumentNullException">Thrown when the given application context or collection of arguments is null.</exception>
-        public RunCommandConsoleApplication(IRunCommandApplicationContext applicationContext, string[] arguments)
-            : base(applicationContext, arguments)
-        {
-            Configure(
-                configuration =>
-                {
-                    configuration.AllowMultipleValues = true;
-                    configuration.AllowSwitches = false;
-                    configuration.Prefixes = CommandPrefixes.ForwardSlash;
-                    configuration.Separators = CommandSeparators.Equals;
-                }
-            );
-        }
+	/// <summary>
+	/// Application used to provide a Run command that can be used to register executables and script files
+	/// that can be run through the use of this application by specifying the registered name.
+	/// </summary>
+	public class RunCommandConsoleApplication : ConsoleApplication<IRunCommandApplicationContext>
+	{
+		/// <summary>
+		/// Initializes a new instance of the <see cref="RunCommandConsoleApplication" /> class.
+		/// </summary>
+		/// <param name="applicationContext">An instance of the context required for the current application.</param>
+		/// <param name="arguments">A collection of all arguments passed through to the application
+		/// from the command line.</param>
+		/// <exception cref="ArgumentNullException">Thrown when the given application context or collection of arguments is null.</exception>
+		public RunCommandConsoleApplication(IRunCommandApplicationContext applicationContext, string[] arguments)
+			: base(applicationContext, arguments)
+		{
+			Configure(
+				configuration =>
+				{
+					configuration.AllowMultipleValues = true;
+					configuration.AllowSwitches = false;
+					configuration.Prefixes = CommandPrefixes.ForwardSlash;
+					configuration.Separators = CommandSeparators.Equals;
+				}
+			);
+		}
 
-        // TODO: It would be nice to be able to register commands with patterns and descriptions so that the a default help command can be provided
-        // TODO: for all commands and each individual command - using the format of "command help" or "command help parameter".
+		// TODO: It would be nice to be able to register commands with patterns and descriptions so that the a default help command can be provided
+		// TODO: for all commands and each individual command - using the format of "command help" or "command help parameter".
 
-        /// <summary>
-        /// Executes the operations contained within the console application implementation.
-        /// </summary>
-        /// <param name="commands">A set of commands parsed from the given command-line arguments.</param>
-        protected override void RunCore(CommandResultSet commands)
-        {
-            HandleRegistration(commands);
-            HandleLaunching(commands);
-        }
+		/// <summary>
+		/// Executes the operations contained within the console application implementation.
+		/// </summary>
+		/// <param name="commands">A set of commands parsed from the given command-line arguments.</param>
+		protected override void RunCore(CommandResultSet commands)
+		{
+			HandleRegistration(commands);
+			HandleLaunching(commands);
+		}
 
-        private void HandleRegistration(CommandResultSet commands)
-        {
-            string[] registrationParameters;
+		private void HandleRegistration(CommandResultSet commands)
+		{
+			string[] registrationParameters;
 
-            if (!commands.TryGetValue<string[]>("register", out registrationParameters))
-            {
-                return;
-            }
+			if (!commands.TryGetValue<string[]>("register", out registrationParameters))
+			{
+				return;
+			}
 
-            if (registrationParameters.Length != 2)
-            {
-                ConsoleHelper.WriteLine(ConsoleMessageType.Error,
-                                        "Invalid number of values specified for command registration. Please specify the name and program path.");
+			if (registrationParameters.Length != 2)
+			{
+				ConsoleHelper.WriteLine(ConsoleMessageType.Error,
+										"Invalid number of values specified for command registration. Please specify the name and program path.");
 
-                return;
-            }
+				return;
+			}
 
-            // Extract the registration name and program path from the registration parameters
-            string registrationName = registrationParameters[0];
-            string programPath = registrationParameters[1];
+			// Extract the registration name and program path from the registration parameters
+			string registrationName = registrationParameters[0];
+			string programPath = registrationParameters[1];
 
-            VerificationProvider.VerifyValidString(registrationName, "registrationName");
-            VerificationProvider.VerifyValidString(programPath, "programPath");
+			VerificationProvider.VerifyValidString(registrationName, "registrationName");
+			VerificationProvider.VerifyValidString(programPath, "programPath");
 
-            // Check we haven't already made a registration with this name
-            // TODO: This information needs to be persisted to some file store.
-            if (ApplicationContext.ProgramRegistrations.ContainsKey(registrationName))
-            {
-                ConsoleHelper.WriteLine(ConsoleMessageType.Warning,
-                                        "A program has already been registered under the name '{0}' and will be overridden.",
-                                        registrationName);
-            }
+			// Check we haven't already made a registration with this name
+			if (ApplicationContext.ProgramRegistrations.ContainsKey(registrationName))
+			{
+				ConsoleHelper.WriteLine(ConsoleMessageType.Warning,
+										"A program has already been registered under the name '{0}' and will be overridden.",
+										registrationName);
+			}
 
-            // Store the registration, overriding any existing registrations
-            ApplicationContext.ProgramRegistrations[registrationName] = programPath;
+			// Store the registration, overriding any existing registrations
+			ApplicationContext.ProgramRegistrations[registrationName] = programPath;
 
-            ConsoleHelper.WriteLine(ConsoleMessageType.Information,
-                                    "A registration has been successfully made under '{0}' for the path '{1}'.",
-                                    registrationName, programPath);
-        }
+			ConsoleHelper.WriteLine(ConsoleMessageType.Information,
+									"A registration has been successfully made under '{0}' for the path '{1}'.",
+									registrationName, programPath);
+		}
 
-        private void HandleLaunching(CommandResultSet commands)
-        {
-            string registrationName;
+		private void HandleLaunching(CommandResultSet commands)
+		{
+			string registrationName;
+			if (!commands.TryGetValue<string>("launch", out registrationName))
+			{
+				return;
+			}
 
-            if (!commands.TryGetValue<string>("launch", out registrationName))
-            {
-                ConsoleHelper.WriteLine(ConsoleMessageType.Warning, "No registration exists under the name '{0}'.",
-                                        registrationName);
+			string programPath;
+			if (!ApplicationContext.ProgramRegistrations.TryGetValue(registrationName, out programPath))
+			{
+				ConsoleHelper.WriteLine(ConsoleMessageType.Warning, "No registration exists under the name '{0}'.",
+										registrationName);
+			}
 
-                return;
-            }
-
-            // Try to launch a new process using the program path registered under the name
-            Process.Start(ApplicationContext.ProgramRegistrations[registrationName]);
-        }
-    }
+			// Try to launch a new process using the program path registered under the name
+			Process.Start(programPath);
+		}
+	}
 }
--- Slade/Slade.Commands/ConsoleApplication.cs
+++ Slade/Slade.Commands/ConsoleApplication.cs
@@ -4,107 +4,109 @@ using System;
 
 namespace Slade.Commands
 {
-    /// <summary>
-    /// Provides an abstract implementation of an application used to handle interaction through the
-    /// use of a command line interface.
-    /// </summary>
-    public abstract class ConsoleApplication<TContext>
-        where TContext : IApplicationContext
-    {
-        private readonly CommandLineParser mParser = new CommandLineParser();
-        private readonly ObjectConverterFactory mObjectConverterFactory = new ObjectConverterFactory();
+	/// <summary>
+	/// Provides an abstract implementation of an application used to handle interaction through the
+	/// use of a command line interface.
+	/// </summary>
+	public abstract class ConsoleApplication<TContext>
+		where TContext : IApplicationContext
+	{
+		private readonly CommandLineParser mParser = new CommandLineParser();
+		private readonly ObjectConverterFactory mObjectConverterFactory = new ObjectConverterFactory();
 
-        private readonly TContext mApplicationContext;
+		private readonly TContext mApplicationContext;
 
-        private readonly string[] mArguments;
-        private CommandResultSet mCommands;
+		private readonly string[] mArguments;
+		private CommandResultSet mCommands;
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="ConsoleApplication" /> class.
-        /// </summary>
-        /// <param name="applicationContext">An instance of the strongly-typed application context.</param>
-        /// <param name="arguments">A collection of all arguments passed through to the application
-        /// from the command line.</param>
-        /// <exception cref="ArgumentNullException">Thrown when the given application context or collection of arguments is null.</exception>
-        protected ConsoleApplication(TContext applicationContext, string[] arguments)
-        {
-            VerificationProvider.VerifyNotNull(applicationContext, "applicationContext");
-            VerificationProvider.VerifyNotNull(arguments, "arguments");
+		/// <summary>
+		/// Initializes a new instance of the <see cref="ConsoleApplication" /> class.
+		/// </summary>
+		/// <param name="applicationContext">An instance of the strongly-typed application context.</param>
+		/// <param name="arguments">A collection of all arguments passed through to the application
+		/// from the command line.</param>
+		/// <exception cref="ArgumentNullException">Thrown when the given application context or collection of arguments is null.</exception>
+		protected ConsoleApplication(TContext applicationContext, string[] arguments)
+		{
+			VerificationProvider.VerifyNotNull(applicationContext, "applicationContext");
+			VerificationProvider.VerifyNotNull(arguments, "arguments");
 
-            mArguments = arguments;
+			mArguments = arguments;
 
-            // Initialize and load the application context
-            mApplicationContext = applicationContext;
-            mApplicationContext.Load();
-        }
+			// Initialize and load the application context
+			mApplicationContext = applicationContext;
+			mApplicationContext.Load();
+		}
 
-        /// <summary>
-        /// Provides access to the strongly-typed context for the current application.
-        /// </summary>
-        protected TContext ApplicationContext
-        {
-            get { return mApplicationContext; }
-        }
+		/// <summary>
+		/// Provides access to the strongly-typed context for the current application.
+		/// </summary>
+		protected TContext ApplicationContext
+		{
+			get { return mApplicationContext; }
+		}
 
-        /// <summary>
-        /// Allows the application to be configured by derived classes.
-        /// </summary>
-        /// <param name="configuration">An action used to configure the rule set being used by the application for
-        /// defining the context parameters for the command-line argument parsing operation.</param>
-        /// <exception cref="ArgumentNullException">Thrown when the given configuration action is null.</exception>
-        protected ConsoleApplication<TContext> Configure(Action<CommandLineRuleSet> configuration)
-        {
-            VerificationProvider.VerifyNotNull(configuration, "configuration");
+		/// <summary>
+		/// Allows the application to be configured by derived classes.
+		/// </summary>
+		/// <param name="configuration">An action used to configure the rule set being used by the application for
+		/// defining the context parameters for the command-line argument parsing operation.</param>
+		/// <exception cref="ArgumentNullException">Thrown when the given configuration action is null.</exception>
+		protected ConsoleApplication<TContext> Configure(Action<CommandLineRuleSet> configuration)
+		{
+			VerificationProvider.VerifyNotNull(configuration, "configuration");
 
-            configuration.Invoke(mParser.RuleSet);
+			configuration.Invoke(mParser.RuleSet);
 
-            return this;
-        }
+			return this;
+		}
 
-        /// <summary>
-        /// Starts execution of the console application.
-        /// </summary>
-        /// <remarks>
-        /// <para>
-        /// If the command-line arguments have not already been parsed by this point, then they will be before
-        /// application execution properly begins.
-        /// </para>
-        /// </remarks>
-        public void Run()
-        {
-            EnsureArgumentsParsed();
+		/// <summary>
+		/// Starts execution of the console application.
+		/// </summary>
+		/// <remarks>
+		/// <para>
+		/// If the command-line arguments have not already been parsed by this point, then they will be before
+		/// application execution properly begins.
+		/// </para>
+		/// </remarks>
+		public void Run()
+		{
+			EnsureArgumentsParsed();
 
-            try
-            {
-                RunCore(mCommands);
-            }
-            catch (Exception ex)
-            {
-                ConsoleHelper.WriteLine(ConsoleMessageType.Error, "Application execution failed:\n{0}", ex.Message);
-            }
-        }
+			try
+			{
+				RunCore(mCommands);
+			}
+			catch (Exception ex)
+			{
+				ConsoleHelper.WriteLine(ConsoleMessageType.Error, "Application execution failed:\n{0}", ex.Message);
+			}
 
-        /// <summary>
-        /// Executes the operations contained within the console application implementation.
-        /// </summary>
-        /// <param name="commands">A set of commands parsed from the given command-line arguments.</param>
-        protected abstract void RunCore(CommandResultSet commands);
+			mApplicationContext.Save();
+		}
 
-        private void EnsureArgumentsParsed()
-        {
-            if (mCommands == null)
-            {
-                try
-                {
-                    var parsedCommands = mParser.Parse(mArguments);
-                    mCommands = new CommandResultSet(mObjectConverterFactory, parsedCommands);
-                }
-                catch (Exception ex)
-                {
-                    ConsoleHelper.WriteLine(ConsoleMessageType.Error, "Failed to parse command-line arguments:\n{0}",
-                                            ex.Message);
-                }
-            }
-        }
-    }
+		/// <summary>
+		/// Executes the operations contained within the console application implementation.
+		/// </summary>
+		/// <param name="commands">A set of commands parsed from the given command-line arguments.</param>
+		protected abstract void RunCore(CommandResultSet commands);
+
+		private void EnsureArgumentsParsed()
+		{
+			if (mCommands == null)
+			{
+				try
+				{
+					var parsedCommands = mParser.Parse(mArguments);
+					mCommands = new CommandResultSet(mObjectConverterFactory, parsedCommands);
+				}
+				catch (Exception ex)
+				{
+					ConsoleHelper.WriteLine(ConsoleMessageType.Error, "Failed to parse command-line arguments:\n{0}",
+											ex.Message);
+				}
+			}
+		}
+	}
 }
